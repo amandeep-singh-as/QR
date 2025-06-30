81a6c910c739d4c41f4968a8219e7ed73917def6
Amandeep <singhamandeep71@gmail.com>
2025-06-30 13:35:36 +0200

    chore reverted commit id changes

--- inject-commit-id.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Replace the commit id placeholder in index.html with the latest commit hash
-COMMIT_ID=$(git rev-parse --short HEAD)
-sed -i.bak "s/COMMIT_ID_PLACEHOLDER/$COMMIT_ID/g" index.html
